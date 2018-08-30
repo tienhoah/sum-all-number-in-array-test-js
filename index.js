178495d02f4354fa00609c49516ea3fdbedf9c58
tienhoah <nickhoang11@gmail.com>
2018-08-30 00:06:50 +0000

    complete sum of array exercise

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let total = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      total += sumItems(item);
+    } else {
+      total += item;
+    }
+  });
+  return total;
 }
 
+
+
 module.exports = sumItems;
+
+
